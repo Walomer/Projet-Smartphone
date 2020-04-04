fcac06a82095a4324b28556ca0d4b2c12802ef34
anpineau <an.pineau12@gmail.com>
2020-04-04 21:00:08 +0200

    Fragments & navigation

---
+++ app/src/main/java/com/example/achassat/myapplication/FragmentVue3.java
@@ -0,0 +1,26 @@
+package com.example.achassat.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentVue3 extends Fragment {
+
+    public FragmentVue3() {
+        // constructeur vide requis ne pas enlever
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_vue3, container, false);
+
+        // mettre les trucs de la vue 3 ici
+
+        return view;
+    } // onCreateView
+
+}
--- app/src/main/java/com/example/achassat/myapplication/MainActivity.java
+++ app/src/main/java/com/example/achassat/myapplication/MainActivity.java
@@ -4,55 +4,85 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.View;
-import android.widget.Button;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
+/*
 
-public class MainActivity extends Activity {
+    Hierarchie de l'appli :
+        - MainActivity
+            > FrameLayout : Layout dans lequel les fragments sont injectes
+            > BottomNavigationView : Barre de navigation en bas de l'appli qui
+                 permet de switch entre les fragments affiches
 
-    Button btnVue1;
-    Button btnVue2;
+            * FragmentVue2 contient lui-même un fragment qui est utilise pour afficher la Map
+                  (c'est la facon normale de googlemap)
+
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // ------------ Ici on declare les fragments et le fragment manager (FragmentVue1 desactive parce qu'il crash)
+    //final Fragment fragmentVue1 = new FragmentVue1();
+    final Fragment fragmentVue2 = new FragmentVue2();
+    final Fragment fragmentVue3 = new FragmentVue3();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragmentVue2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        setTitle("Projet android");
 
-        // Création du listener du bouton permetant d'afficher la vue 1
-        btnVue1 =(Button)findViewById(R.id.buttonVue1);
-        btnVue1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent vue1 = new Intent(MainActivity.this, ActivityVue1.class);
-                startActivity(vue1);
+        // ---------- On recupere la barre de navigation en bas et on lui assigne un ItemListener qu'on cree plus bas
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        /* ---------- On demarre tous les fragments et on cache (avec hide) ceux qui sont pas selectionnes
+            dans la barre de navigation, ca permet de conserver les modifications sur un fragment quand on en change.
+            Exemple: si on va a un endroit sur une map, qu'on change de fragment et qu'on revient sur la map plus
+            tard, l'endroit sur la map sera conserve car le fragment n'a jamais ete ferme, seulement cache      */
+
+        // FragmentVue1 toujours desactive car il crash
+        //fm.beginTransaction().add(R.id.fragmentcontainer, fragmentVue1, "1").hide(fragmentVue1).commit();
+        fm.beginTransaction().add(R.id.fragmentcontainer, fragmentVue3, "3").hide(fragmentVue3).commit();
+        fm.beginTransaction().add(R.id.fragmentcontainer, fragmentVue2, "2").commit();
+
+    } // onCreate
+
+    // --------- Le navigation item listener utilise plus haut pour la barre de navigation
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_vue2:
+                    fm.beginTransaction().hide(active).show(fragmentVue2).commit();
+                    active = fragmentVue2;
+                    return true;
+
+                case R.id.navigation_vue3:
+                    fm.beginTransaction().hide(active).show(fragmentVue3).commit();
+                    active = fragmentVue3;
+                    return true;
+
+                case R.id.navigation_vue1:
+                    /*fm.beginTransaction().hide(active).show(fragmentVue3).commit();
+                    active = fragmentVue3;*/
+                    Toast.makeText(getApplicationContext(), "Vue 1 désactivée", Toast.LENGTH_SHORT).show();
+                    return true;
             }
-        });
-
-        //Création du listener du bouton permetant d'afficher la vue 1
-        btnVue2 =(Button)findViewById(R.id.buttonVue2);
-        btnVue2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent vue2 = new Intent(MainActivity.this, ActivityVue2.class);
-                startActivity(vue2);
-            }
-        });
-
-        if (android.os.Build.VERSION.SDK_INT > 9)
-        {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
+            return false;
         }
+    }; // Navigation Item Listener
 
-        //Connection a NMEA simulator
-        try {
-            new Connection("86.209.9.188", 3000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
 }
+
